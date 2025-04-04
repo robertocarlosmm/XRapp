82cffdb7377d88bc6dadf063416b50953c429615
Roberto Carlos Maldonado Morán <a20171016@pucp.edu.pe>
2025-04-03 22:05:32 -0500

    primera escena

--- src/main.js
+++ src/main.js
@@ -1,2 +1,11 @@
-import * as BABYLON from "babylonjs"
-console.log(BABYLON)
+import { Engine, Scene } from "babylonjs";
+import { startScene } from "./scenes/start";
+
+const engine = new Engine(document.querySelector("canvas"), true);
+
+async function main(){
+    const scene = await startScene(engine);
+    engine.runRenderLoop( ()=>scene.render() )
+}
+
+main();
